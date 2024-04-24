cee771415ed991f891ee2de8bcfc35100ebc460b
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-04-24 09:55:03 +0200

    Added a simple component for the homescreen

--- src/main.c
+++ src/main.c
@@ -1,48 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
 #include "util/sdl.h"
-#include "util/render.h"
-
-// Test Function to exit the game loop
-char quitted_game()
-{
-  SDL_Event event;
-  char quitted_game = 0;
-
-  while (SDL_PollEvent(&event))
-  {
-    switch (event.type)
-    {
-    case SDL_QUIT:
-      quitted_game = 1;
-      break;
-    case SDL_KEYDOWN:
-      if (event.key.keysym.sym == SDLK_ESCAPE)
-      {
-        quitted_game = 1;
-        break;
-      }
-    default:
-      break;
-    }
-  }
-
-  return quitted_game;
-}
+#include "component/homescreen.h"
 
 int main()
 {
   init_sdl();
-  // Test Game Loop
-  long then = SDL_GetTicks();
-  float remainder = 0;
-  while (!quitted_game())
-  {
-    cap_frame_rate(&then, &remainder, 30);
-    prepare_scene();
-    render_text("Welcome to Caladan!", (vector_2d_t){.x = 90, .y = 300}, (SDL_Color){255, 255, 255, 255});
-    present_scene();
-  }
+  // Moving to the homescreen main loop
+  homescreen();
   return EXIT_SUCCESS;
 }
--- src/util/render.h
+++ src/util/render.h
@@ -6,5 +6,6 @@
 
 void blit(SDL_Rect dest, SDL_Texture *texture);
 void render_text(char *text, vector_2d_t pos, SDL_Color color);
+void render_animated_text(char *text, vector_2d_t pos, SDL_Color color, int anima_val);
 
 #endif
